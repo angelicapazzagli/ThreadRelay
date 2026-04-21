46073cee4fa29dfd550c3c62357ace59812a93ed
angelicapazzagli <pazzagli.angelica@franchettisalviani.net>
2026-04-21 10:33:17 +0200

    Logica classe di scambio Testimone

--- RelayRunners/src/relayrunners/Testimone.java
+++ RelayRunners/src/relayrunners/Testimone.java
@@ -9,5 +9,20 @@ package relayrunners;
  * @author PAZZAGLI.ANGELICA
  */
 public class Testimone {
-    private boolean attivo;
+    private boolean inCorsa;
+    
+    public synchronized void corri() throws InterruptedException {
+        while(inCorsa) {
+            System.out.println("Attesa testimone");
+            wait();
+        }
+        inCorsa = true;
+        System.out.println("Entrata in corsa");
+    }
+    
+    public synchronized void passa() {
+        inCorsa = false;
+        System.out.println("Passaggio testimone");
+        notifyAll();
+    }
 }
